3c4267c230119bbf8a73d3f2471ffc28f0882792
CJ Mariani <marianic0@students.rowan.edu>
2014-08-03 17:18:27 -0400

    Added button support.

--- utils/intls/Input.hx
+++ utils/intls/Input.hx
@@ -1,6 +1,8 @@
 package utils.intls;
+import w2d.Wumbo;
 
 import openfl.events.KeyboardEvent;
+import openfl.events.MouseEvent;
 
 class Input
 {
@@ -8,6 +10,7 @@ class Input
 	static private var charCode:Int;
 	static private var held:Bool = false;
 	static private var x:Int;
+	static private var click:Bool = false;
 	
 	
 	public function new() 
@@ -19,6 +22,14 @@ class Input
 		});
 		flash.Lib.current.stage.addEventListener( KeyboardEvent.KEY_UP, function(e:KeyboardEvent) {
 			held = false;
+		});
+		
+		flash.Lib.current.stage.addEventListener(MouseEvent.MOUSE_DOWN, function(e:MouseEvent) {
+			click = true;
+		});
+		
+		flash.Lib.current.stage.addEventListener(MouseEvent.MOUSE_UP, function(e:MouseEvent) {
+			click = false;
 		});
 	}
 	
@@ -33,5 +44,17 @@ class Input
 	}
 	
 	public function getFullCode() { }
+	
+	public function mouseClicked() {
+		return click;
+	}
+	
+	public function mouseX() {
+		return Wumbo.grid.toGridX(flash.Lib.current.mouseX);
+	}
+	
+	public function mouseY() {
+		return Wumbo.grid.toGridY(flash.Lib.current.mouseY);
+	}
 	
 }
