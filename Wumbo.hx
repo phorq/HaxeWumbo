3de8ec29737fa5a87d151c951e13367308de4b98
CJ Mariani <marianic0@students.rowan.edu>
2014-12-10 14:51:56 -0500

    Fixed merge problems

--- Wumbo.hx
+++ Wumbo.hx
@@ -1,40 +1,3 @@
-<<<<<<< HEAD
-package w2d;
-
-import flash.events.Event;
-typedef Button = utils.intls.Button;
-
-typedef Line = utils.Line;
-typedef Text = utils.Text;
-typedef Rectangle = utils.Rectangle;
-typedef Grid = utils.intls.Grid;
-typedef Input = utils.intls.Input;
-
-class Wumbo {
-	
-	static public var update:Float->Void = null;
-	private var timer:Float = 0;
-	
-	// Loads base utils
-	static public var grid:			Grid = new Grid();
-	static public var input:		Input;
-	static public var line:			Line = new Line(grid);
-	static public var text:			Text = new Text(grid);
-	static public var rectangle:	Rectangle = new Rectangle(grid);
-	static public var button:		Button = new Button();
-	
-	public function new(w:Int, h:Int) {
-		input = new Input();
-		grid.setRatio(w, h);
-		flash.Lib.current.addEventListener(Event.ENTER_FRAME, function(e:Event) {
-			flash.Lib.current.removeChildren();
-			grid.setSize(flash.Lib.current.stage.stageWidth, flash.Lib.current.stage.stageHeight);
-			Wumbo.update((flash.Lib.getTimer() - timer) * .001);
-			timer = flash.Lib.getTimer();
-		});
-		
-	}
-=======
 package w2d;
 
 import flash.display.Stage;
@@ -92,5 +55,4 @@ class Wumbo{
 		});
 		
 	}
->>>>>>> workingOnIt
 }
